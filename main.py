fe344204ac6c28a5157014ac372abea29f414f92
Admin <misk.2437@gmail.com>
2023-05-25 17:27:36 +0300

    pluss commit

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 print('привет!\nпогладь котю: он очень хочет этого')
 print('серьёзно!\nгладь сейчас же!')
 print('а кот-то ещё не поглажен...\nГЛАДЬ СЕЙЧАС ЖЕ!!!!')
+from tkinter import *
+impotr
+root = Tk()
+root.title("METANIT.COM")
+root.geometry("800x800")
+
+canvas = Canvas(bg="white", width=700, height=700)
+canvas.pack(anchor=CENTER, expand=1)
+
+root.mainloop()
