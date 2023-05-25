b523bc36dfd2104f1ddacabd688687c68fa2b55d
Admin <misk.2437@gmail.com>
2023-05-25 17:44:22 +0300

    pluss commit turtle

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ print('привет!\nпогладь котю: он очень хочет это
 print('серьёзно!\nгладь сейчас же!')
 print('а кот-то ещё не поглажен...\nГЛАДЬ СЕЙЧАС ЖЕ!!!!')
 from tkinter import *
-impotr
+import turtle as t
+t.color('#714B23')
+t.forward(29)
 root = Tk()
 root.title("METANIT.COM")
 root.geometry("800x800")
 
-canvas = Canvas(bg="white", width=700, height=700)
+canvas = Canvas(bg="white", width=10, height=10)
 canvas.pack(anchor=CENTER, expand=1)
 
 root.mainloop()
